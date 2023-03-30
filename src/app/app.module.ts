7949f52bc6e5682c73606f00a0be15f49cab8bf4
Sabrina-barreiros <83960592+Sabrina-barreiros@users.noreply.github.com>
2023-03-29 21:28:24 -0300

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RootComponent } from './root/root.component';
 import {BrowserModule} from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './content/footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule
   ],
-  declarations: [],
+  declarations: [RootComponent,
+  HeaderComponent,
+  ContentComponent,
+  FooterComponent],
   bootstrap: [RootComponent]
 })
 export class AppModule { }
